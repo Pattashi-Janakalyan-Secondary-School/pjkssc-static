405059ea5bc44f26d90002f076ff896e2a7cf9b7
Jobayer Arman <carbonjha@gmail.com>
2016-07-20 10:27:10 +0600

    update 2.0.0
    #HTML
    + navigation & header
    + carousel
    + font page
    + footer
    + template pages init
    #CSS
    + image mixin center
    + body background
    + dynamic h1, h2, h3
    - carousel gradient
    + carousel font-awesome
    + navbar justified
    + navs padding

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,19 @@
-$(document).ready(function(){
-	// write scripts here
+// Scroll to Top Button
+$(function scrolltotop() {
+  var offset = 150,
+  scrollbutton = $('.scroll-top');
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > offset) {
+      scrollbutton.fadeIn('slow');
+    } else{
+      scrollbutton.fadeOut('fast');
+    }
+  });
+
+  scrollbutton.children('a').on('click', function(event) {
+    event.preventDefault();
+    $('html, body').animate({scrollTop: 0}, 800);
+    return false;
+  });
 });
